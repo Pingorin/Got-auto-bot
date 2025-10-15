2d4e84d7af7c0a29eac3ccc4a703993f311ada7a
Pingorin <125970228+Pingorin@users.noreply.github.com>
2025-10-16 00:21:59 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,5 @@
+API_ID = 12345678  # my.telegram.org से
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token_from_botfather"
+CHANNEL_ID = -1001234567890  # Channel का ID (negative)
+DB_NAME = "files.db"  # Database file
